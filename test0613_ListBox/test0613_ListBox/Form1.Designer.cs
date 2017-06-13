c483093be9a7668d94fda34a0a0c89f163936caa
vul35al6 <vul35al6@gmail.com>
2017-06-13 21:01:31 +0800

    Add files via upload

---
+++ test0613_ListBox/test0613_ListBox/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace test0613_ListBox {
+    partial class Form1 {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 19;
+            this.listBox.Location = new System.Drawing.Point(12, 21);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(315, 251);
+            this.listBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 448);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 79);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "增加項目";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(119, 278);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 79);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "移除項目";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(226, 363);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 79);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "全部清除";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(119, 363);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 79);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "替換項目";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 363);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(101, 79);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "按鈕文字測試";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 278);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(101, 79);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "跳選框文字測試";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(226, 278);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(101, 79);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "顯示選取項目";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(119, 448);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(101, 79);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "顯示選取項目順位1.";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(226, 448);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(101, 79);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "顯示選取項目順位2.";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Location = new System.Drawing.Point(422, 21);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(315, 251);
+            this.listBox1.TabIndex = 10;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 19;
+            this.listBox2.Location = new System.Drawing.Point(743, 21);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(315, 251);
+            this.listBox2.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1237, 542);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox);
+            this.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ test0613_ListBox/test0613_ListBox/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test0613_ListBox {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            listBox.Items.Add("你好你好");
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            listBox.Items.Remove("你好你好");           
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            listBox.Items.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+            listBox.Items[2]= "謝謝您的惠顧";
+        }
+
+        private void button5_Click(object sender, EventArgs e) {
+            button5.Text = ("OK");
+        }
+
+        private void button6_Click(object sender, EventArgs e) {
+            MessageBox.Show("第一次實驗");
+        }
+
+        private void button7_Click(object sender, EventArgs e) {
+            button7.Text = listBox.SelectedItem.ToString();
+        }
+
+        private void button8_Click(object sender, EventArgs e) {
+            button8.Text = listBox.SelectedIndex.ToString();
+        }
+
+        private void button9_Click(object sender, EventArgs e) {
+            int i = listBox.SelectedIndex;
+            if (i < 0)
+                button9.Text = ("沒選到喔!");
+            else
+                button9.Text = listBox.Items[i].ToString();
+
+        }
+    }
+}
